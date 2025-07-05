ee94d9091bcac809774f00f01af3bd687946aad2
Neelam <neelamkhan1008@gmail.com>
2025-07-05 10:58:28 +0530

    Update API URL to point to live backend

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,10 @@ const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json({ limit: '10mb' }));
-app.use(cors({ origin: 'http://localhost:4200' }));
+app.use(cors({
+  origin: ['http://localhost:4200', 'https://e-admin-dashboard.netlify.app']
+}));
+
 
 app.use('/api/products', productRoutes);
 app.use('/uploads', express.static(path.join(__dirname, '../uploads'))); // serve uploads from backend/uploads
